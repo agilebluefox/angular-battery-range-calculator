dba34f37ae07be58f663a484924a5d02b14567b6
David Conner <agilebluefox@gmail.com>
2017-01-15 15:47:41 -0500

    Setup the method to produce the stats
    The data is a complex object returned from the service. The stats will be dependent on the user selections in the form. To start, the default values will be used, but should be modified as the selections change the parameters.

--- src/app/tesla-battery/tesla-battery.component.ts
+++ src/app/tesla-battery/tesla-battery.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup } from '@angular/forms';
 
+import { Stat } from '../models/stat.interface';
+import { TeslaBatteryService } from '../tesla-battery/tesla-battery.service';
+
 @Component({
   selector: 'app-tesla-battery',
   templateUrl: './tesla-battery.component.html',
@@ -9,11 +12,21 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 export class TeslaBatteryComponent implements OnInit {
 
   title: string = 'Range Per Charge';
+  models: any;
+  stats: Stat[];
   tesla: FormGroup;
 
-  constructor(public fb: FormBuilder) { }
+  private results: Array<String> = ['60', '60D', '75', '75D', '90D', 'P100D'];
+
+  constructor(
+    public fb: FormBuilder,
+    private teslaBatteryService: TeslaBatteryService
+    ) { }
 
   ngOnInit() {
+
+    this.models = this.teslaBatteryService.getModelData();
+
     this.tesla = this.fb.group({
       config: this.fb.group({
         speed: 55,
@@ -22,6 +35,20 @@ export class TeslaBatteryComponent implements OnInit {
         wheels: 19
       })
     });
+
+    this.stats = this.calculateStats(this.results, this.tesla.controls['config'].value);
+
   }
+
+  private calculateStats(models, value): Stat[]  {
+  return models.map(model => {
+    const { speed, temperature, climate, wheels } = value;
+    const miles = this.models[model][wheels][climate ? 'on' : 'off'].speed[speed][temperature];
+    return {
+      model,
+      miles
+    };
+  });
+}
 
 }
